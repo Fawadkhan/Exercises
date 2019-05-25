22cbacf2651a3d48d91975d10c2a07d00873c390
Fawad khan <fovadkhan@outlook.com>
2019-05-25 09:04:16 +0500

    exercise 2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import { profiles, users, movies } from "./data.js";
 import "./App.css";
 import FavoriteMovies from "./FavouriteMovies.js";
-import popularMovies from "./PopularMovies.js";
+import PopularMovies from "./PopularMovies.js";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <FavoriteMovies profiles={profiles} users={users} movies={movies} />
+        <PopularMovies profiles={profiles} users={users} movies={movies} />
       </div>
     );
   }
--- src/PopularMovies.js
+++ src/PopularMovies.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import UserLiked from "./UserLiked";
 
-class popularMovies extends Component {
-  state = {};
+class PopularMovies extends Component {
   render() {
-    return <div />;
+    const { profiles, users, movies } = this.props;
+    const moviesArr = Object.values(movies);
+    return (
+      <div className="PopularMovies-container">
+        {moviesArr.map(movie => (
+          <div key={movie.id}>
+            <h3>{movie.name}</h3>
+            <UserLiked movieID={movie.id} users={users} profiles={profiles} />
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
-export default popularMovies;
+export default PopularMovies;
