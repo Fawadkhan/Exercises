b6412ea279f4164ba185292e8fd95d83c1eda26f
Fawad khan <fovadkhan@outlook.com>
2019-05-26 01:12:24 +0500

    SFC

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import "./App.css";
 import FavoriteMovies from "./FavouriteMovies.js";
 import PopularMovies from "./PopularMovies.js";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <PopularMovies profiles={profiles} users={users} movies={movies} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <PopularMovies profiles={profiles} users={users} movies={movies} />
+    </div>
+  );
+};
 
 export default App;
--- src/PopularMovies.js
+++ src/PopularMovies.js
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
 import UserLiked from "./UserLiked";
 
-class PopularMovies extends Component {
-  render() {
-    const { profiles, users, movies } = this.props;
-    const moviesArr = Object.values(movies);
-    return (
-      <div className="PopularMovies-container">
-        {moviesArr.map(movie => (
-          <div key={movie.id}>
-            <h3>{movie.name}</h3>
-            <UserLiked movieID={movie.id} users={users} profiles={profiles} />
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+const PopularMovies = props => {
+  const { profiles, users, movies } = props;
+  const moviesArr = Object.values(movies);
+  return (
+    <div className="PopularMovies-container">
+      {moviesArr.map(movie => (
+        <div key={movie.id}>
+          <h3>{movie.name}</h3>
+          <UserLiked movieID={movie.id} users={users} profiles={profiles} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default PopularMovies;
